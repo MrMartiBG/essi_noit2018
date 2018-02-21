ac9c311c863669670c244022750d5866e9cf7f79
nnivan <ivannikolov007@gmail.com>
2018-02-21 02:45:34 +0200

    connection to db

---
+++ config/configure_database.js
@@ -0,0 +1,7 @@
+var db_conf = {};
+
+db_conf.configure = function(connection){
+	console.log("connection")
+};
+
+module.exports = db_conf;
--- config/database_user.js
+++ config/database_user.js
@@ -1,5 +1,5 @@
 module.exports = {
 	host: "192.168.1.104",
 	user: "root",
-	password: ""
+	password: "1234asdf"
 }
