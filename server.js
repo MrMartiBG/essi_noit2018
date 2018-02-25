f4506856c47d659a53bd0626c55ae3981d4d56a2
Ivan <ivannikolov007@gmail.com>
2018-02-25 16:15:41 +0200

    socket in new file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var socket = require('./socket.js');
 var mysql = require('mysql');
 var db_user = require('./config/database_user.js');
 var connection = require('./config/configure_database.js')(mysql,db_user);
@@ -56,106 +57,7 @@ app.get('/styles.ac89bfdd6de82636b768.bundle.css', function(req, res){
 
 var data_array = [];
 
-io.on('connection', function(socket){
-  console.log('user connected', socket.id);
-  console.log('user remoteAddress',socket.request.connection.remoteAddress);
-  socket.authenticated = false;
-
-  socket.register_user_fail = function register_user_fail(code){
-    socket.emit('registration fail');
-    console.log('registration fail', code);
-    return code;
-  }
-  socket.register_user_successful = function register_user_successful(user){
-    socket.authenticated = true;
-    socket.user_name = user.user_name;
-    socket.emit('registration successful');
-    console.log('registration successful', socket.user_name);
-    return user;
-  }
-
-  socket.login_user_fail = function login_user_fail(code){
-    socket.emit('login fail');
-    console.log('login fail', code);
-    return code;
-  }
-  socket.login_user_successful = function login_user_successful(user){
-    socket.authenticated = true;
-    socket.user_name = user.user_name;
-    socket.emit('login successful');
-    console.log('login successful', socket.user_name);
-    return user;
-  }
-
-  socket.logout_user_fail = function logout_user_fail(code){
-    socket.emit('logout fail');
-    console.log('logout fail', code);
-    return code;
-  }
-  socket.logout_user_successful = function logout_user_successful(){
-    socket.authenticated = false;
-    socket.emit('logout successful');
-    console.log('logout successful', socket.user_name);
-    return true;
-  }
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('register user', function(user){
-    if(socket.authenticated) return socket.register_user_fail(0);
-    console.log('socket.on register user', user);
-    if( user.user_name!=null  && user.user_name!='' &&
-        user.password!=null   && user.password!='' &&
-        user.email!=null      && user.email!='' ){
-      db.register_user(user, function(err, results){
-        if(err){
-          socket.register_user_fail(2);
-        }else{
-          socket.register_user_successful(user);
-        }
-      });
-    }else{
-      socket.register_user_fail(3);
-    }
-  });
-
-  socket.on('login user', function(user){
-    if(socket.authenticated) return socket.login_user_fail(0);
-    console.log('socket.on login user', user);
-    if( user.user_name!=null  && user.user_name!='' &&
-        user.password!=null   && user.password!=''){
-      db.login_user(user, function(err, results){
-        if(err){
-          socket.login_user_fail(2);
-        }else{
-          if(results[0].password == user.password){
-            socket.login_user_successful(user);
-          }else{
-            socket.login_user_fail(1);
-          }
-        }
-      });
-    }else{
-      socket.login_user_fail(3);
-    }
-  });
-
-  socket.on('logout user', function(){
-    if(!socket.authenticated){
-      return socket.logout_user_fail(0);
-    }else{
-      socket.logout_user_successful();
-    }
-    console.log('socket.on logout user', socket.user_name);
-  });
-
-  socket.on("new-message", function(msg){
-    console.log("new-message", msg);
-  });
-
-});
+io.on('connection', socket);
 
 http.listen(3030, function(){
   console.log('Server started! At http://localhost:3030');
