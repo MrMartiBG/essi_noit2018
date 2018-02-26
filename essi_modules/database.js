6499662f270928bf3846110f5c55e02bccaadc70
nnivan <ivannikolov007@gmail.com>
2018-02-26 21:37:16 +0200

    fix register (NOT TESTED)

--- essi_modules/database.js
+++ essi_modules/database.js
@@ -1,26 +1,17 @@
 var db  = function(connection){
+
 	this.register_user = function register_user(user, func){
-		connection.query('INSERT INTO users SET ?',user, func);
+		connection.query('INSERT INTO user SET ?',user, func);
 	}
-
 	this.fetch_user = function fetch_user(username, func){
-		connection.query("SELECT * FROM users WHERE username = '" + username + "'", func);
+		connection.query("SELECT * FROM user WHERE username = '" + username + "'", func);
 	}
 
-	this.register_car = function register_car(car, func){
-		connection.query('INSERT INTO cars SET ?',car, func);
+	this.add_user_info = function add_user_info(user_info, func){
+		connection.query('INSERT INTO user_info SET ?',user_info, func);
 	}
-
-	this.fetch_cars = function fetch_cars(owner, func){
-		connection.query("SELECT * FROM cars WHERE owner = '" + owner + "'", func);
-	}
-
-	this.register_modification = function register_modification(modification, func){
-		connection.query('INSERT INTO modifications SET ?',modification, func);
-	}
-
-	this.fetch_modifications = function fetch_modifications(car_id, func){
-		connection.query("SELECT * FROM modifications WHERE car_id = '" + car_id + "'", func);
+	this.fetch_user_info = function fetch_user_info(user_id, func){
+		connection.query("SELECT * FROM user_info WHERE user_id = '" + user_id + "'", func);
 	}
 
 	return this;
--- essi_modules/sockets/login.js
+++ essi_modules/sockets/login.js
@@ -1,91 +1,112 @@
 module.exports = function(socket,database){
 
-  socket.authenticated = false;
+	socket.authenticated = false;
 
-  socket.register_user_fail = function register_user_fail(code){
-    socket.emit('registration fail', code);
-    console.log('registration fail', code);
-    return code;
-  }
-  socket.register_user_successful = function register_user_successful(user){
-    socket.authenticated = true;
-    socket.username = user.username;
-    socket.emit('registration successful');
-    console.log('registration successful', socket.username);
-    return user;
-  }
+	socket.register_user_fail = function register_user_fail(code){
+		socket.emit('registration_fail', code);
+		console.log('registration_fail', code);
+		return code;
+	}
+	socket.register_user_successful = function register_user_successful(user, user_info){
+		socket.authenticated = true;
+		socket.username = user.username;
+		socket.email = user.email;
+		socket.firstname = user_info.firstname;
+		socket.lastname = user_info.lastname;
 
-  socket.login_user_fail = function login_user_fail(code){
-    socket.emit('login fail', code);
-    console.log('login fail', code);
-    return code;
-  }
-  socket.login_user_successful = function login_user_successful(user){
-    socket.authenticated = true;
-    socket.username = user.username;
-    socket.emit('login successful');
-    console.log('login successful', socket.username);
-    return user;
-  }
+		socket.emit('registration_successful');
+		console.log('registration_successful', socket.username);
+		
+		return user;
+	}
+	socket.on('register_user', function(user, user_info){
+		console.log('socket.on register_user', user, user_info);
 
-  socket.logout_user_fail = function logout_user_fail(code){
-    socket.emit('logout fail', code);
-    console.log('logout fail', code);
-    return code;
-  }
-  socket.logout_user_successful = function logout_user_successful(){
-    socket.authenticated = false;
-    socket.emit('logout successful');
-    console.log('logout successful', socket.username);
-    return true;
-  }
+		if(socket.authenticated) return socket.register_user_fail(0);
 
-  socket.on('register user', function(user){
-    console.log('socket.on register user', user);
-    if(socket.authenticated) return socket.register_user_fail(0);
-    if( user.username!=null  && user.username!='' &&
-        user.password!=null   && user.password!='' &&
-        user.email!=null      && user.email!='' ){
-      database.register_user(user, function(err, results){
-        if(err){
-          socket.register_user_fail(2);
-        }else{
-          socket.register_user_successful(user);
-        }
-      });
-    }else{
-      socket.register_user_fail(3);
-    }
-  });
+		if( user_info.firstname!=null   && user_info.firstname!='' &&
+			user_info.lastname!=null   && user_info.lastname!='' &&
+			user.username!=null  && user.username!='' &&
+			user.password!=null   && user.password!='' &&
+			user.email!=null      && user.email!='' ){
+			
+			database.register_user(user, function(err, results){
+				if(err){
+					return socket.register_user_fail(3);
+				}else{
+					console.log(results);
+					database.add_user_info(user_info, function(err, results){
+						if(err){
+							return socket.register_user_fail(4)
+						}else{
+							return socket.register_user_successful(user);
 
-  socket.on('login user', function(user){
-    console.log('socket.on login user', user);
-    if(socket.authenticated) return socket.login_user_fail(0);
-    if( user.username!=null  && user.username!='' &&
-        user.password!=null   && user.password!=''){
-      database.fetch_user(user.username, function(err, results){
-        if(err){
-          socket.login_user_fail(2);
-        }else{
-          if(results[0].password == user.password){
-            socket.login_user_successful(user);
-          }else{
-            socket.login_user_fail(1);
-          }
-        }
-      });
-    }else{
-      socket.login_user_fail(3);
-    }
-  });
+						}
+					});
+				}
+			});
+		}else{
+			return socket.register_user_fail(2);
+		}
+	});
 
-  socket.on('logout user', function(){
-    console.log('socket.on logout user', socket.username);
-    if(!socket.authenticated){
-      return socket.logout_user_fail(0);
-    }else{
-      socket.logout_user_successful();
-    }
-  });
+
+
+	socket.login_user_fail = function login_user_fail(code){
+		socket.emit('login_fail', code);
+		console.log('login_fail', code);
+		return code;
+	}
+	socket.login_user_successful = function login_user_successful(user){
+		socket.authenticated = true;
+		socket.username = user.username;
+		socket.emit('login_successful');
+		console.log('login_successful', socket.username);
+		return user;
+	}
+	socket.on('login_user', function(user){
+		console.log('socket.on login_user', user);
+
+		if(socket.authenticated) return socket.login_user_fail(0);
+
+		if( user.username!=null  && user.username!='' &&
+				user.password!=null   && user.password!=''){
+			database.fetch_user(user.username, function(err, results){
+				if(err){
+					return socket.login_user_fail(3);
+				}else{
+					if(results[0].password == user.password){
+						return socket.login_user_successful(user);
+					}else{
+						return socket.login_user_fail(1);
+					}
+				}
+			});
+		}else{
+			socket.login_user_fail(2);
+		}
+	});
+
+
+
+	socket.logout_user_fail = function logout_user_fail(code){
+		socket.emit('logout_fail', code);
+		console.log('logout_fail', code);
+		return code;
+	}
+	socket.logout_user_successful = function logout_user_successful(){
+		socket.authenticated = false;
+		socket.emit('logout_successful');
+		console.log('logout_successful', socket.username);
+		return true;
+	}
+	socket.on('logout_user', function(){
+		console.log('socket.on logout_user', socket.username);
+		if(!socket.authenticated){
+			return socket.logout_user_fail(0);
+		}else{
+			return socket.logout_user_successful();
+		}
+	});
 
 }
