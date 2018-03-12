18a8df3d6cda444a486d45d4c0dc2419d99d8c14
nnivan <ivannikolov007@gmail.com>
2018-03-13 01:36:18 +0200

    node config_database.js done!

--- configure_database_new.js
+++ configure_database_new.js
@@ -20,11 +20,12 @@ connection.connect(function(err) {
 			console.log("Error: ",	err.code);
 			process.exit(1);
 		}
-		console.log("Database `" + db_name + "` created");
+		console.log("Database `" + db_name + "` created!");
 
 		connection.end();
 
 		db_user.database = db_name;
+		db_user.multipleStatements = true;
 
 		connection = mysql.createConnection(db_user);
 		connection.connect(function(err) {
@@ -43,7 +44,7 @@ connection.connect(function(err) {
 function config_tables(){
 
 	// table user
-	var sql_table_user = "	CREATE TABLE IF NOT EXISTS `user` (								\
+	var sql_table_user = "	CREATE TABLE `user` (								\
 							`id` 			int 			NOT NULL 	AUTO_INCREMENT,		\
 							`username` 		varchar(64) 	NOT NULL 	UNIQUE,				\
 							`password` 		varchar(256)	NOT NULL,						\
@@ -55,7 +56,7 @@ function config_tables(){
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
 	// table car
-	var sql_table_car = "	CREATE TABLE IF NOT EXISTS `car` ( 								\
+	var sql_table_car = "	CREATE TABLE `car` ( 								\
 							`id` 			int 			NOT NULL 	AUTO_INCREMENT, 	\
 							`owner_id` 		int 			NOT NULL, 						\
 							`public` 		bool 						DEFAULT false, 		\
@@ -68,7 +69,7 @@ function config_tables(){
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
 	// table service_info
-	var sql_table_service_info = "	CREATE TABLE IF NOT EXISTS `service_info` ( 			\
+	var sql_table_service_info = "	CREATE TABLE `service_info` ( 			\
 							`id` 			int 			NOT NULL 	AUTO_INCREMENT, 	\
 							`name` 			varchar(64) 	NOT NULL, 						\
 							`address` 		varchar(64) 	NOT NULL, 						\
@@ -78,20 +79,20 @@ function config_tables(){
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
 	// table service_user
-	var sql_table_service_user = "	CREATE TABLE IF NOT EXISTS `service_user` ( 			\
+	var sql_table_service_user = "	CREATE TABLE `service_user` ( 			\
 							`service_id` 	int 			NOT NULL, 						\
 							`user_id` 		int 			NOT NULL, 						\
 							`user_type` 	varchar(64) 	NOT NULL 						\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
 	// table service_car
-	var sql_table_service_car = "	CREATE TABLE IF NOT EXISTS `service_car` ( 				\
+	var sql_table_service_car = "	CREATE TABLE `service_car` ( 				\
 							`service_id` 	int 			NOT NULL, 						\
 							`car_id` 		int 			NOT NULL						\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
 	// table modification
-	var sql_table_modification = "	CREATE TABLE IF NOT EXISTS `modification` ( 			\
+	var sql_table_modification = "	CREATE TABLE `modification` ( 			\
 							`id` 			int 			NOT NULL 	AUTO_INCREMENT, 	\
 							`car_id` 		int 			NOT NULL, 						\
 							`service_id` 	int 			NOT NULL, 						\
@@ -104,47 +105,18 @@ function config_tables(){
 							PRIMARY KEY (`id`)			 									\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
-
-	connection.query(sql_table_user, function (err, result) {
+	connection.query(	sql_table_user + 
+						sql_table_car + 
+						sql_table_service_info + 
+						sql_table_service_user + 
+						sql_table_service_car + 
+						sql_table_modification, 
+	function (err, result) {
 		if (err){
 			console.log("Error: ",	err.code);
 			process.exit(1);
 		}
-	    console.log("Table `user` done!");
-	});
-	connection.query(sql_table_car, function (err, result) {
-		if (err){
-			console.log("Error: ",	err.code);
-			process.exit(1);
-		}
-	    console.log("Table `car` done!");
-	});
-	connection.query(sql_table_service_info, function (err, result) {
-		if (err){
-			console.log("Error: ",	err.code);
-			process.exit(1);
-		}
-	    console.log("Table `service_info` done!");
-	});
-	connection.query(sql_table_service_user, function (err, result) {
-		if (err){
-			console.log("Error: ",	err.code);
-			process.exit(1);
-		}
-	    console.log("Table `service_user` done!");
-	});
-	connection.query(sql_table_service_car, function (err, result) {
-		if (err){
-			console.log("Error: ",	err.code);
-			process.exit(1);
-		}
-	    console.log("Table `service_car` done!");
-	});			
-	connection.query(sql_table_modification, function (err, result) {
-		if (err){
-			console.log("Error: ",	err.code);
-			process.exit(1);
-		}
-	    console.log("Table `modification` done!");
+	    console.log("Tables created!");
+	    connection.end();
 	});
 }
