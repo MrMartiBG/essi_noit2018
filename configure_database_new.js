350faa9ddc7edca2764ed2ca7e01b9f08ac20ef1
nnivan <ivannikolov007@gmail.com>
2018-03-13 01:27:43 +0200

    new database_config

--- configure_database_new.js
+++ configure_database_new.js
@@ -1,12 +1,46 @@
-var db_conf  = function(mysql,db_user){
+var      mysql = require('mysql');
+var    db_user = require('./config/database_user_new.js');
 
-	console.log("configure");
+var db_name = 'essi';
 
-	var connection = mysql.createConnection(db_user)
-	connection.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected to DATABASE!");
+
+console.log("Config database...");
+
+connection = mysql.createConnection(db_user);
+connection.connect(function(err) {
+
+	if (err){
+		console.log("Error: ",	err.code);
+		process.exit(1);
+	}
+	console.log("Connected to mysql!");
+
+	connection.query("CREATE DATABASE " + db_name , function (err, result) {
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
+		console.log("Database `" + db_name + "` created");
+
+		connection.end();
+
+		db_user.database = db_name;
+
+		connection = mysql.createConnection(db_user);
+		connection.connect(function(err) {
+			if (err){
+				console.log("Error: ",	err.code);
+				process.exit(1);
+			}
+			console.log("Connected to database!");
+			config_tables();
+			
+		});
 	});
+});
+
+
+function config_tables(){
 
 	// table user
 	var sql_table_user = "	CREATE TABLE IF NOT EXISTS `user` (								\
@@ -16,7 +50,7 @@ var db_conf  = function(mysql,db_user){
 							`email` 		varchar(64) 	NOT NULL 	UNIQUE,				\
 							`firstname` 	varchar(64) 	NOT NULL,						\
 							`lastname` 		varchar(64) 	NOT NULL, 						\
-							`mobile` 		varchar(64) 									\
+							`mobile` 		varchar(64), 									\
 							PRIMARY KEY (`id`) 												\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
@@ -29,7 +63,7 @@ var db_conf  = function(mysql,db_user){
 							`model` 		varchar(64)  	NOT NULL, 						\
 							`generation` 	varchar(64)  	NOT NULL, 						\
 							`engine` 		varchar(64)  	NOT NULL, 						\
-							`vin_number` 	varchar(64)  	NOT NULL 	UNIQUE				\
+							`vin_number` 	varchar(64)  	NOT NULL 	UNIQUE,				\
 							PRIMARY KEY (`id`) 												\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
@@ -58,7 +92,7 @@ var db_conf  = function(mysql,db_user){
 
 	// table modification
 	var sql_table_modification = "	CREATE TABLE IF NOT EXISTS `modification` ( 			\
-							`id` 			int 			NOT NULL AUTO_INCREMENT, 		\
+							`id` 			int 			NOT NULL 	AUTO_INCREMENT, 	\
 							`car_id` 		int 			NOT NULL, 						\
 							`service_id` 	int 			NOT NULL, 						\
 							`status` 		varchar(64), 									\
@@ -66,40 +100,51 @@ var db_conf  = function(mysql,db_user){
 							`date` 			datetime		NOT NULL, 						\
 							`type` 			varchar(64) 	NOT NULL, 						\
 							`part` 			varchar(64) 	NOT NULL, 						\
-							`description` 	varchar(256) 	NOT NULL 						\
+							`description` 	varchar(256) 	NOT NULL, 						\
 							PRIMARY KEY (`id`)			 									\
 						) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"					;
 
-			
 
 	connection.query(sql_table_user, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `user` done!");
-	 });
+	});
 	connection.query(sql_table_car, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `car` done!");
-	 });
+	});
 	connection.query(sql_table_service_info, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `service_info` done!");
-	 });
+	});
 	connection.query(sql_table_service_user, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `service_user` done!");
-	 });
+	});
 	connection.query(sql_table_service_car, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `service_car` done!");
-	 });			
+	});			
 	connection.query(sql_table_modification, function (err, result) {
-	    if (err) throw err;
+		if (err){
+			console.log("Error: ",	err.code);
+			process.exit(1);
+		}
 	    console.log("Table `modification` done!");
-	 });
-
-
-
-	return connection;
-};
-
-module.exports = db_conf;
+	});
+}
