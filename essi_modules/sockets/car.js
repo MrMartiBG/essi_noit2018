15fac355cff28f78f8b4990d28ea7b8ca0c0b6ab
MrMartiBG <martirace@gmail.com>
2018-03-21 19:12:50 +0200

    fetch_car_service added

--- essi_modules/sockets/car.js
+++ essi_modules/sockets/car.js
@@ -48,38 +48,38 @@ module.exports = function(socket,database,validation){
 	});
 
 
-	// socket.on('fetch_car_service', function(info, call_back){ // info
-	// 	console.log('socket.on fetch_car_service', info);
-	// 	if(!socket.arguments_valid(info, call_back)) return false;
-	// 	if(!socket.authenticated) return socket.fail("fetch_car_service", {code: 101}, call_back);
-	//
-	// 	car = {
-	// 		id: info.id
-	// 	};
-	// 	service_car = {
-	// 		car_id: info.id
-	// 	};
-	// 	service_user = {
-	// 		user_id: socket.user.id
-	// 	};
-	//
-	// 	database.fetch_service_user(service_user, function(err, results){
-	// 		if(err)
-	// 			return socket.fail("fetch_car_service", {code: 201}, call_back);
-	// 		service_user = results;
-	// 		database.fetch_service_car(service_car, function(err, results){
-	// 			if(err)
-	// 				return socket.fail("fetch_car_service", {code: 202}, call_back);
-	// 			service_car = results;
-	// 			if(!validation.service_car_service_user_in(service_car, service_user))
-	// 				return socket.fail("fetch_car_service", {code: 103}, call_back);
-	// 			database.fetch_car(car, function(err, results){
-	// 				if(err) return socket.fail("fetch_car_service", {code: 203}, call_back);
-	// 				return socket.successful("fetch_car_service", results, call_back);
-	// 			});
-	// 		});
-	// 	});
-	// });
+	socket.on('fetch_car_service', function(info, call_back){ // info
+		console.log('socket.on fetch_car_service', info);
+		if(!socket.arguments_valid(info, call_back)) return false;
+		if(!socket.authenticated) return socket.fail("fetch_car_service", {code: 101}, call_back);
+
+		car = {
+			id: info.id
+		};
+		service_car = {
+			car_id: info.id
+		};
+		service_user = {
+			user_id: socket.user.id
+		};
+
+		database.fetch_service_user(service_user, function(err, results){
+			if(err)
+				return socket.fail("fetch_car_service", {code: 201}, call_back);
+			service_user = results;
+			database.fetch_service_car(service_car, function(err, results){
+				if(err)
+					return socket.fail("fetch_car_service", {code: 202}, call_back);
+				service_car = results;
+				if(!validation.service_car_service_user_in(service_car, service_user))
+					return socket.fail("fetch_car_service", {code: 103}, call_back);
+				database.fetch_car(car, function(err, results){
+					if(err) return socket.fail("fetch_car_service", {code: 203}, call_back);
+					return socket.successful("fetch_car_service", results, call_back);
+				});
+			});
+		});
+	});
 
 	socket.on('fetch_car_public', function(info, call_back){ // info: id
 		console.log('socket.on fetch_car_public', info);
