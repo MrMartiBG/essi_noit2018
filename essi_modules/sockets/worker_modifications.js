592d4b8a333c38e3537c1c0232a3aba413f5c549
nnivan <ivannikolov007@gmail.com>
2018-05-07 18:30:46 +0300

    get_car_modifications_by_worker addded

--- essi_modules/sockets/worker_modifications.js
+++ essi_modules/sockets/worker_modifications.js
@@ -1,33 +1,33 @@
 module.exports = function(socket,database){
 
 
-	// socket.on('get_car_modifications_by_worker', function(info, call_back){
+	socket.on('get_car_modifications_by_worker', function(info, call_back){
 		
-	// 	socket.validate_worker_rights("get_car_modifications_by_worker", info, call_back, 0b1000, function(func_name, info, call_back){
+		socket.validate_worker_rights("get_car_modifications_by_worker", info, call_back, 0b1000, function(func_name, info, call_back){
 
-	// 		if(info.car_id == undefined) return socket.fail(func_name, {errmsg: "car_id is undefined"}, call_back);
+			if(info.car_id == undefined) return socket.fail(func_name, {errmsg: "car_id is undefined"}, call_back);
 
-	// 		var car = {
-	// 			car_id: info.car_id
-	// 		}
-	// 		var service = {
-	// 			account_service_id: info.account_service_id
-	// 		}
+			var car = {
+				car_id: info.car_id
+			}
+			var service = {
+				account_service_id: info.account_service_id
+			}
 
-	// 		database.get_service_car_by_worker(car, service, function(err, results){
-	// 			if(err) return socket.fail(func_name, {errmsg: "database error get_service_car_by_worker", code: err}, call_back);
-	// 			if(results.length == 0) return socket.fail(func_name, {errmsg: "no match service and car in service_car"}, call_back);
+			database.get_service_car_by_worker(car, service, function(err, results){
+				if(err) return socket.fail(func_name, {errmsg: "database error get_service_car_by_worker", code: err}, call_back);
+				if(results.length == 0) return socket.fail(func_name, {errmsg: "no match service and car in service_car"}, call_back);
 
-	// 			database.get_modifications_by_worker(car, function(err, results){
-	// 				if(err) return socket.fail(func_name, {errmsg: "database error get_modifications_by_worker", code: err}, call_back);
+				database.get_modifications_by_worker(car, function(err, results){
+					if(err) return socket.fail(func_name, {errmsg: "database error get_modifications_by_worker", code: err}, call_back);
 
-	// 				return socket.successful(func_name, results, call_back);
-	// 			});
-	// 		});
+					return socket.successful(func_name, results, call_back);
+				});
+			});
 
-	// 	});
+		});
 
-	// });
+	});
 
 
 	socket.on('add_car_modification_by_worker', function(info, call_back){
