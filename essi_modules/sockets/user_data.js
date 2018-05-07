f1513494fadfdd9b2d737a3808ff71746e386f66
nnivan <ivannikolov007@gmail.com>
2018-05-07 13:34:43 +0300

    get_service_users_this_user added

--- essi_modules/sockets/user_data.js
+++ essi_modules/sockets/user_data.js
@@ -3,30 +3,34 @@ module.exports = function(socket,database){
 
 	socket.on('get_data_this_user', function(info, call_back){
 
+		var func_name = "get_data_this_user";
+
 		console.log('socket.on get_data_this_user', info);
 		if(!socket.arguments_valid(info, call_back)) return false;
 
-		if(!socket.authenticated) return socket.fail("get_data_this_user", {errmsg: "You are not in account"}, call_back);
-		if(socket.account.type != "user") return socket.fail("get_data_this_user", {errmsg: "You are not user"}, call_back);
+		if(!socket.authenticated) return socket.fail(func_name, {errmsg: "You are not in account"}, call_back);
+		if(socket.account.type != "user") return socket.fail(func_name, {errmsg: "You are not user"}, call_back);
 
 		var user = 	{
 			account_id:	socket.account.id
 		};
 
 		database.get_user_data(user, function(err, results){
-			if(err) return socket.fail("get_data_this_user", {errmsg: "database error get_user_data", code: err.code}, call_back);
-			return socket.successful("get_data_this_user", results[0], call_back);
+			if(err) return socket.fail(func_name, {errmsg: "database error get_user_data", code: err.code}, call_back);
+			return socket.successful(func_name, results[0], call_back);
 		});
 
 	});
 
 	socket.on('set_data_this_user', function(info, call_back){
 
+		var func_name = "set_data_this_user";
+
 		console.log('socket.on set_data_this_user', info);
 		if(!socket.arguments_valid(info, call_back)) return false;
 
-		if(!socket.authenticated) return socket.fail("set_data_this_user", {errmsg: "You are not in account"}, call_back);
-		if(socket.account.type != "user") return socket.fail("set_data_this_user", {errmsg: "You are not user"}, call_back);
+		if(!socket.authenticated) return socket.fail(func_name, {errmsg: "You are not in account"}, call_back);
+		if(socket.account.type != "user") return socket.fail(func_name, {errmsg: "You are not user"}, call_back);
 
 		var user = 	{
 			account_id:	socket.account.id
@@ -43,9 +47,30 @@ module.exports = function(socket,database){
 		if(info.info != undefined) new_user.info = info.info;
 
 		database.set_user_data(new_user, user, function(err, results){
-			if(err) return socket.fail("set_data_this_user", {errmsg: "database error set_user_data", code: err.code}, call_back);
-			return socket.successful("set_data_this_user", new_user, call_back);
+			if(err) return socket.fail(func_name, {errmsg: "database error set_user_data", code: err.code}, call_back);
+			return socket.successful(func_name, new_user, call_back);
 		});
 	});
 
+
+	socket.on('get_service_users_this_user', function(info, call_back){
+
+		var func_name = "get_service_users_this_user";
+
+		console.log('socket.on get_service_users_this_user', info);
+		if(!socket.arguments_valid(info, call_back)) return false;
+
+		if(!socket.authenticated) return socket.fail(func_name, {errmsg: "You are not in account"}, call_back);
+		if(socket.account.type != "user") return socket.fail(func_name, {errmsg: "You are not user"}, call_back);
+
+		var user = 	{
+			account_user_id: socket.account.id
+		};
+
+		database.get_service_user(user, function(err, results){
+			if(err) return socket.fail(func_name, {errmsg: "database error get_service_user", code: err.code}, call_back);
+			return socket.successful(func_name, results, call_back);
+		});
+
+	});
 }
